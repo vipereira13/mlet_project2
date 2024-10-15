017acb03e8c155348362bb890afb45eb3d457591
Edson Baião <edson.baiao@gmail.com>
2024-10-15 17:01:24 -0300

    add: Adjust S3 to include the date partition

--- src/utils/upload.py
+++ src/utils/upload.py
@@ -5,13 +5,15 @@ from src.utils.functions import caminho
 def preparatoupload():
     file_path = caminho()
     day = lastbussinessday()
-    day = day.replace('-', '')
-    df = pd.read_csv(file_path + f'/TradeInformationConsolidatedFile_{day}_1.csv', skiprows=1, sep=';')
+    day_nodash = day.replace('-', '')
+    df = pd.read_csv(f"{file_path}/TradeInformationConsolidatedFile_{day_nodash}_1.csv", skiprows=1, sep=';')
     df = df.fillna(0)
 
-    s3_url = 's3://bucket/folder/bucket.parquet.gzip'
-    df.to_parquet(s3_url, compression='gzip')\
+    # Adjust the S3 URL to include the date partition
+    s3_url = f's3://b3-project/raw-data/date={day}/TradeInformationConsolidatedFile_{day_nodash}_1.parquet.gzip'
+
+    # Save the DataFrame to S3 in Parquet format with gzip compression
+    df.to_parquet(s3_url, compression='gzip')
 
     return df
-
 
